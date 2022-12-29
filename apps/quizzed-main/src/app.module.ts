054068bf242a097cdd18e10ae891728f14ec78e3
Krishna Mahato <krishhtrishh9304@gmail.com>
2022-12-29 12:30:33 +0530

    ADD: Docker compose and misc config

--- apps/quizzed-main/src/app.module.ts
+++ apps/quizzed-main/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/quizzed-main/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,3 @@
-export * from './common.module';
-export * from './common.service';
+export * from './database/database.module';
+export * from './database/abstract.repository';
+export * from './database/abstract.schema';
