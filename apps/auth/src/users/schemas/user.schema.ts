d1b4d0e2b4db69b4d5d8d212ede354478c571caa
Krishna Mahato <krishhtrishh9304@gmail.com>
2023-01-05 02:03:17 +0530

    ADD: User schema

--- apps/auth/src/users/schemas/user.schema.ts
+++ apps/auth/src/users/schemas/user.schema.ts
@@ -1,13 +1,47 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { AbstractDocument } from '@app/common';
+import { IsEmail, IsMobilePhone, IsStrongPassword } from 'class-validator';
 
 @Schema({ versionKey: false })
 export class User extends AbstractDocument {
-  @Prop()
+  @Prop({ required: true, length: 10 })
+  regdNo: string;
+
+  @Prop({ required: true })
+  @IsEmail()
   email: string;
 
-  @Prop()
+  @Prop({ required: true })
+  @IsStrongPassword({
+    minLength: 8,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   password: string;
+
+  @Prop({ required: true })
+  @IsMobilePhone('en-IN')
+  phone: string;
+
+  @Prop({ required: true })
+  semester: string;
+
+  @Prop({ required: true })
+  branch: string;
+
+  @Prop({ default: 'active' })
+  status: string;
+
+  @Prop({ default: new Date().toDateString() })
+  created_at: string;
+
+  @Prop({ default: new Date().toDateString() })
+  updated_at: string;
+
+  @Prop({ default: null })
+  metadata: any;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
